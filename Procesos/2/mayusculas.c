906ced292b776927d6029718422033b7cedcbcfe
chaarlie37 <carlos37sm@gmail.com>
2019-10-30 18:14:42 +0100

    mayusculas hecho

--- Procesos/2/mayusculas.c
+++ Procesos/2/mayusculas.c
@@ -3,6 +3,10 @@
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
 
 
 int main(int argc, char const *argv[]) {
@@ -19,24 +23,27 @@ int main(int argc, char const *argv[]) {
         close(pipe_2[0]);
         read(pipe_1[0], buff, 1024);
         char buffcaps[1024];
-        for(int i = 0; buff[i] != "\0"; i++){
+        int i = 0;
+        while(buff[i] != '\0'){
             buffcaps[i] = toupper(buff[i]);
+            i++;
         }
+        buffcaps[i] = '\0';
         write(pipe_2[1], buffcaps, 1024);
         close(pipe_2[1]);
         close(pipe_1[0]);
     }else{
         char buff[1024];
         char buffcaps[1024];
-        fgets(buff, 1024, stdin);
         close(pipe_1[0]);
         close(pipe_2[1]);
+        fgets(buff, 1024, stdin);
         write(pipe_1[1], buff, 1024);
         read(pipe_2[0], buffcaps, 1024);
+        fputs(buffcaps, stdout);
         close(pipe_1[1]);
         close(pipe_2[0]);
         wait(NULL);
-        fputs(buffcaps, stdout);
     }
     return 0;
 }
